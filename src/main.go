49496f9ffbd0549bd08a710045fe979c88c7cffc
x12xx12x <44411062+12xx12@users.noreply.github.com>
2023-10-20 23:29:33 +0200

    added handling for shutdown request

--- src/main.go
+++ src/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	// Create a channel to listen for signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	// Handle signals
+	go func() {
+		<-sigChan
+		// Perform graceful shutdown or cleanup actions here
+		StopServer()
+		// For example, save data, close connections, etc.
+		// Then, exit the application
+		os.Exit(0)
+	}()
+
+	// Your application code here
 }
