6ab9daf4d8c35e68babeebf8804a64a5a3808f8f
x12xx12x <44411062+12xx12@users.noreply.github.com>
2023-10-23 18:37:25 +0200

    use string instead of uuid

--- pkg/registry/device.go
+++ pkg/registry/device.go
@@ -3,10 +3,10 @@ package registry
 import "gorm.io/gorm"
 
 type Device struct {
-	Id           string `json:"id" gorm:"primaryKey;type:uuid;"`
-	Name         string `json:"name" gorm:"not null"`
-	Manufacturer string `json:"manufacturer" gorm:"type:uuid;not null"`
-	Description  string `json:"description"`
+	Id           string `json:"id" gorm:"primaryKey;type:string; not null"`
+	Name         string `json:"name" gorm:"type: string; not null"`
+	Manufacturer string `json:"manufacturer" gorm:"type:string;not null"`
+	Description  string `json:"description" gorm:"type:string"`
 	Quantity     int    `json:"quantity" gorm:"not null; check:quantity > 0"`
 	Contents     string `json:"contents"`
 	gorm.Model
