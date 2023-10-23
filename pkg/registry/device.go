41fd61f77df350b66a05937304e3ab8b2a9312c7
x12xx12x <44411062+12xx12@users.noreply.github.com>
2023-10-23 22:58:10 +0200

    removed type hints

--- pkg/registry/device.go
+++ pkg/registry/device.go
@@ -3,10 +3,10 @@ package registry
 import "gorm.io/gorm"
 
 type Device struct {
-	Id           string `json:"id" gorm:"primaryKey;type:string; not null"`
-	Name         string `json:"name" gorm:"type: string; not null"`
-	Manufacturer string `json:"manufacturer" gorm:"type:string;not null"`
-	Description  string `json:"description" gorm:"type:string"`
+	Id           string `json:"id" gorm:"primaryKey; not null"`
+	Name         string `json:"name" gorm:"not null"`
+	Manufacturer string `json:"manufacturer" gorm:"not null"`
+	Description  string `json:"description"`
 	Quantity     int    `json:"quantity" gorm:"not null; check:quantity > 0"`
 	Contents     string `json:"contents"`
 	gorm.Model
